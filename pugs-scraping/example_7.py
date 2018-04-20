865221078ad66a1f6a56bff7d0b4bebaf1af43bd
jonreiter@gmail.com <jonreiter@gmail.com>
2018-04-20 16:51:55 +0800

    add a huge python line to illustrate

--- pugs-scraping/example_7.py
+++ pugs-scraping/example_7.py
@@ -1,6 +1,7 @@
 
 import os
 import tempfile
+import datetime
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,3 +19,6 @@ driver.get(url)
 
 dateField = driver.find_element_by_id('UpdatedTextCDR')
 print(dateField.text)
+
+# or, cause it's python....
+dt = datetime.datetime.strptime("-".join( [[str(i) for i in driver.find_element_by_id('UpdatedTextCDR').text.split(' ')[2].split('/')][j] for j in [2,0,1]] ),"%Y-%m-%d")
