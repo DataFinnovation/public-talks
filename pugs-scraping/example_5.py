c7ec79f58048606f8ceb3cca14bbe0c1cbbe2f4c
jonreiter@gmail.com <jonreiter@gmail.com>
2018-04-25 14:45:03 +0800

    simplify

--- pugs-scraping/example_5.py
+++ pugs-scraping/example_5.py
@@ -7,20 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 # start up a chrome
 driver = webdriver.Chrome()
 
-# laod our target page
+# load our target page
 url = 'https://cdr.ffiec.gov/public/PWS/DownloadBulkData.aspx'
 driver.get(url)
 
-# set the type in a form
-productForm = driver.find_element_by_xpath("//select[contains(@name,'ctl00$MainContentHolder$ListBox1')]")
-formSelect = Select(productForm)
-formSelect.select_by_visible_text('Call Reports -- Single Period')
-
-# now click a button
-radioButton = driver.find_element_by_id('XBRLRadiobutton')
-radioButton.click()
-
-# find the download button and click that too
+# find the download button and click
 button = driver.find_element_by_name('ctl00$MainContentHolder$TabStrip1$Download_0')
 button.click()
 
