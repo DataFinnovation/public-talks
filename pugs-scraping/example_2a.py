fb7028d17df552e4314532393aae7bf7e46502c0
jonreiter@gmail.com <jonreiter@gmail.com>
2018-04-25 11:25:15 +0800

    relative navigation example

---
+++ pugs-scraping/example_2a.py
@@ -0,0 +1,27 @@
+
+import lxml.etree
+import requests
+
+# target location
+url = 'https://raw.githubusercontent.com/DataFinnovation/public-talks/master/pugs-scraping/example3.html'
+
+# get the page
+page = requests.get(url)
+
+# parse it
+tree = lxml.etree.fromstring(page.content)
+
+# what elements we care about
+theXPath = '//td[@class="name" and text() = "Apple"]'
+thePriceXPath = '../td[@class="price"]/text()'
+theColorXPath = '../td[@class="color"]/text()'
+
+# get the apple element
+appleEle = tree.xpath(theXPath)[0]
+price = appleEle.xpath(thePriceXPath)[0]
+color = appleEle.xpath(theColorXPath)[0]
+
+# now grab one of those links
+print("price is: " + price)
+print("color is: " + color)
+
